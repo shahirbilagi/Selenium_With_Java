ec4f09345f74140e2a596ff488089fc539e83f46
Shahir Bilagi <135726167+shahirbilagi@users.noreply.github.com>
2025-02-21 18:14:01 +0530

    Basic Locators

--- Locators.java
+++ Locators.java
@@ -15,25 +15,22 @@ public class Locators {
         driver.get("https://github.com/signup?source=login");
 
         //Locators by ID
-        //WebElement Username = driver.findElement(By.id("email"));
-        //Username.sendKeys("Shahir@gmail.com");
-        //WebElement password = driver.findElement(By.id("password"));
-        //password.sendKeys("Shair@1234");
+        WebElement Username = driver.findElement(By.id("email"));
+        Username.sendKeys("Shahir@gmail.com");
+        WebElement password = driver.findElement(By.id("password"));
+        password.sendKeys("Shair@1234");
 
         //Locators by ID
-        // WebElement UN = driver.findElement(By.id("login"));
-        // UN.sendKeys("shahirbilagi");
+        WebElement UN = driver.findElement(By.id("login"));
+        UN.sendKeys("shahirbilagi");
 
         //Locator by LinkText
         driver.findElement(By.linkText("Sign in →")).click();
         driver.findElement(By.name("password")).sendKeys(".......");
 
+        //Locator by CSS Selector
         driver.findElement(By.cssSelector("input.form-control input[name=login]")).sendKeys("shahirbilagi");
         driver.findElement(By.name("commit")).click();
-
-
-
-
 
     }
 }
