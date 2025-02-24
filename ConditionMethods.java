acc5d2c06d3c89d562d82f02fa5b08e89f121235
Shahir Bilagi <shahirbilagi@Shahirs-MacBook-Pro.local>
2025-02-24 12:03:51 +0530

    Get and Conditional Methods Example

---
+++ ConditionMethods.java
@@ -0,0 +1,41 @@
+package Selenium_With_Java;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ConditionMethods {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+         driver.get("https://www.skechers.in/");
+
+        // IsDisplay Conditional Methods
+        WebElement logo=driver.findElement(By.xpath("//img[@class='sketchers-logo']"));
+        System.out.println("Display of Logo:"+logo.isDisplayed());
+
+        // IsDisplay Conditional Methods direct allocation
+        boolean logoo=driver.findElement(By.xpath("//img[@class='sketchers-logo']")).isDisplayed();
+        if(logo.isDisplayed()){
+            System.out.println("Logo is Displayed");
+        }
+        // IsEnabled Conditional Methods
+        boolean searchbox=driver.findElement(By.xpath("//div[@class='d-none d-lg-block']//input[@placeholder='Search']")).isEnabled();
+        if(searchbox){
+            System.out.println("Searchbox is Enabled");
+        }
+        driver.close();
+
+        //Used Different Web Page for Testing isSelected Methods
+        //IsSelected Conditional Methods
+        driver.get("https://demo.nopcommerce.com/register?returnUrl=%2F");
+        Thread.sleep(3000);
+        WebElement male=driver.findElement(By.xpath("//input[@id='gender-male']"));
+        WebElement female=driver.findElement(By.xpath("//input[@id='gender-female']"));
+        System.out.println("Before Using IsSelected Method for both M&F:"+male.isSelected()+" "+female.isSelected());
+        male.click();
+        System.out.println("After Selecting Male:"+male.isSelected());
+        driver.close();
+    }
+}
---
+++ GetMethods.java
@@ -0,0 +1,48 @@
+package Selenium_With_Java;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+import java.util.Set;
+
+public class GetMethods {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.redbus.in/");
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        // Get Title
+        String title = driver.getTitle();
+        System.out.println("Current Page Title: " + title);
+
+        // Get Current URL
+        String currentUrl = driver.getCurrentUrl();
+        System.out.println("Current URL: " + currentUrl);
+
+        // Get Window Handle
+        String windowHandle = driver.getWindowHandle();
+        System.out.println("Window ID: " + windowHandle);
+
+        // Wait until the element is clickable
+        WebElement newArrivals = wait.until(ExpectedConditions.elementToBeClickable(By.className("name_rb_secondary_item")));
+
+        // Click the element
+        newArrivals.click();
+
+        Thread.sleep(5000); // Allow time for the new window to open
+
+        // Get all window handles
+        Set<String> handles=driver.getWindowHandles();
+        System.out.println("Handles: " + handles);
+
+        // Close the new tab and switch back to the original window
+        driver.quit();
+
+    }
+}
